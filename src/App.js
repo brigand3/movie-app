e61b4aed47154b572a788a2461f5ac550c7ad4a8
Hyunjin Yun <sayconclusion@gmail.com>
2018-12-24 17:01:39 +0900

    Author hjyun\nDelete logo image\nUse fetch to download movie json

--- src/App.js
+++ src/App.js
@@ -11,32 +11,10 @@ class App extends Component {
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster: "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L.jpg"
-          },
-          {
-            title: "The Shawshank Redemption",
-            poster: "https://t1.daumcdn.net/cfile/tistory/214B5B4658266BBE28"
-          },
-          {
-            title: "The Hitman's Bodyguard",
-            poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/4/48/%ED%82%AC%EB%9F%AC%EC%9D%98_%EB%B3%B4%EB%94%94%EA%B0%80%EB%93%9C.jpg/250px-%ED%82%AC%EB%9F%AC%EC%9D%98_%EB%B3%B4%EB%94%94%EA%B0%80%EB%93%9C.jpg"
-          },
-          {
-            title: "Edge of Tomorrow",
-            poster: "https://images-na.ssl-images-amazon.com/images/G/01/digital/video/hero/Movies/2014/EdgeofTomorrow_4926700_883316896709._V344884602_RI_SX940_.jpg"
-          },
-          {
-            title: "Snowpiercer",
-            poster: "https://pmcdeadline2.files.wordpress.com/2014/07/snowpiercer-train-scene.png?w=446&h=299&crop=1"
-          }
-        ]
-      })
-    }, 3000)
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.log(error))
   }
 
   _renderMovies = () => {
