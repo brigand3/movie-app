10b787d08c455fead74f08ea65224323c510f772
Hyunjin Yun <sayconclusion@gmail.com>
2018-12-25 23:51:41 +0900

    Author hjyun\nUse async/await for calling movie data

--- src/App.js
+++ src/App.js
@@ -11,17 +11,29 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(error => console.log(error))
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map(movie => {
+      console.log(movie)
+      return <Movie title={movie.title} poster={movie.medium_cover_image} key={movie.id} />
     })
     return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(error => console.log(error))
   }
 
   render() {
