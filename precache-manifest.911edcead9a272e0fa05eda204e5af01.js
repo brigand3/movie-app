26a7a0febde2e32f3dcbce926391f8774f516dcb
Hyunjin Yun <sayconclusion@gmail.com>
2018-12-26 01:25:30 +0900

    Updates

---
+++ precache-manifest.911edcead9a272e0fa05eda204e5af01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "2cb4e28f6e28ff0b3f3c",
+    "url": "/movie-app/static/js/main.2cb4e28f.chunk.js"
+  },
+  {
+    "revision": "af0846f34c26a370a1d2",
+    "url": "/movie-app/static/js/1.af0846f3.chunk.js"
+  },
+  {
+    "revision": "2cb4e28f6e28ff0b3f3c",
+    "url": "/movie-app/static/css/main.72dc71e3.chunk.css"
+  },
+  {
+    "revision": "15faa7fc58279f3ee5f5860932b9b665",
+    "url": "/movie-app/index.html"
+  }
+];
